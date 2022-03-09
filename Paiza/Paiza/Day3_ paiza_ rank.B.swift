b491d2560ca743b8c59410762382d1696bd42d71
KyusokLee <kyusok1@naver.com>
2022-03-09 20:57:40 +0900

    Day 3 Paiza Study スキルチェック問題 - (3)

---
+++ Paiza/Paiza/Day3_ paiza_ rank.B.swift
@@ -0,0 +1,8 @@
+//
+//  Day3_ paiza_ rank.B.swift
+//  Paiza
+//
+//  Created by Kyus'lee on 2022/03/09.
+//
+
+import Foundation
--- Paiza/Paiza/main.swift
+++ Paiza/Paiza/main.swift
@@ -86,49 +86,214 @@ import Foundation
 //    }
 //}
 
-//山の頂上を探せ Rank.B
-let mapSize = Int(readLine()!)!
-var mapData = [[Int]]()
-let directionRow = [0, 0, -1, 1]
-let directionColumn = [1, -1, 0, 0]
-var mountain_top = [Int]()
+////山の頂上を探せ Rank.B
+//let mapSize = Int(readLine()!)!
+//var mapData = [[Int]]()
+//let directionRow = [0, 0, -1, 1]
+//let directionColumn = [1, -1, 0, 0]
+//var mountain_top = [Int]()
+//
+//for _ in 0..<mapSize {
+//    mapData += [readLine()!.split(separator: " ").map { Int(String($0))! }]
+//}
+//
+//for i in 0..<mapSize {
+//    for j in 0..<mapSize {
+//        if Find_Top(i, j) {
+//            mountain_top.append(mapData[i][j])
+//        }
+//    }
+//}
+//
+//mountain_top.sort(by: >)
+//
+//for i in 0..<mountain_top.count {
+//    print(mountain_top[i])
+//}
+//
+//func Find_Top(_ rowStart: Int, _ columnStart: Int) -> Bool {
+//    let compareValue = mapData[rowStart][columnStart]
+//    var isTop = true
+//
+//    for i in 0..<4 {
+//        let nextRow = rowStart + directionRow[i]
+//        let nextColumn = columnStart +  directionColumn[i]
+//
+//        if nextRow < 0 || nextRow >= mapSize || nextColumn < 0 || nextColumn >= mapSize {
+//            continue
+//        }
+//
+//        let compareRound = mapData[nextRow][nextColumn]
+//
+//        if compareValue <= compareRound {
+//            isTop = false
+//        }
+//    }
+//
+//    return isTop
+//}
 
-for _ in 0..<mapSize {
-    mapData += [readLine()!.split(separator: " ").map { Int(String($0))! }]
-}
+////マッピングゲーム　rank.B
+//// 非常に難しい問題
+//// 配列のIndexの処理に常に注意をすること！　Indexのエラーを探すのに結構時間がかかってしまった。
+//// もっと早く解決できたはず
+//let data = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let turn_Count = data[0], ySize = data[1], xSize = data[2]
+//var board_Size = Array(repeating: Array(repeating: false, count: xSize), count: ySize)
+//var red_Have = Array(repeating: Array(repeating: false, count: xSize), count: ySize)
+//var blue_Have = Array(repeating: Array(repeating: false, count: xSize), count: ySize)
+//var green_Have = Array(repeating: Array(repeating: false, count: xSize), count: ySize)
+//let playersCount = 3
+//var red_Count = 0, blue_Count = 0, green_Count = 0
+//
+//for _ in 0..<turn_Count {
+//    for i in 0..<playersCount {
+//        let putData = readLine()!.split(separator: " ").map { Int(String($0))! }
+//        let x = putData[0], y = putData[1], massSize = putData[2]
+//
+//        putColor(i, x, y, massSize)
+//    }
+//}
+//
+//for i in 0..<ySize {
+//    for j in 0..<xSize {
+//        if red_Have[i][j] {
+//            red_Count += 1
+//        } else if blue_Have[i][j] {
+//            blue_Count += 1
+//        } else if green_Have[i][j] {
+//            green_Count += 1
+//        }
+//    }
+//}
+//
+//print("\(red_Count) \(blue_Count) \(green_Count)")
+//
+//func putColor(_ player: Int, _ xStart: Int, _ yStart: Int, _ mass: Int) {
+//    for i in yStart..<(yStart + mass) {
+//        guard i < ySize else {
+//            return
+//        }
+//        for j in xStart..<(xStart + mass) {
+//            guard j < xSize else {
+//                break
+//                //ここをreturnにしたせいで、passできなかった
+//            }
+//
+//            if board_Size[i][j] {
+//                checkingColor(player, i, j)
+//            } else {
+//                board_Size[i][j] = true
+//                if player == 0 {
+//                   red_Have[i][j] = true
+//                } else if player == 1 {
+//                    blue_Have[i][j] = true
+//                } else {
+//                   green_Have[i][j] = true
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//func checkingColor(_ player: Int, _ y: Int, _ x: Int) {
+//    if player == 0 {
+//        if blue_Have[y][x] {
+//            blue_Have[y][x] = false
+//            green_Have[y][x] = true
+//        } else if green_Have[y][x] {
+//            green_Have[y][x] = false
+//            blue_Have[y][x] = true
+//        } else if red_Have[y][x] {
+//            return
+//        }
+//
+//    } else if player == 1 {
+//        if red_Have[y][x] {
+//            red_Have[y][x] = false
+//            green_Have[y][x] = true
+//        } else if green_Have[y][x] {
+//            green_Have[y][x] = false
+//            red_Have[y][x] = true
+//        } else if blue_Have[y][x] {
+//            return
+//        }
+//
+//    } else {
+//        if red_Have[y][x] {
+//            red_Have[y][x] = false
+//            blue_Have[y][x] = true
+//        } else if blue_Have[y][x] {
+//            blue_Have[y][x] = false
+//            red_Have[y][x] = true
+//        } else if green_Have[y][x] {
+//            return
+//        }
+//    }
+//
+//}
 
-for i in 0..<mapSize {
-    for j in 0..<mapSize {
-        if Find_Top(i, j) {
-            mountain_top.append(mapData[i][j])
-        }
+//ネットサーフィン rank.B
+// indexの扱いが複雑だった問題
+// 結構難しい問題 >> 文字列の処理が弱点であることを再び思い知った
+//⚠️途中の段階
+//‼️完全に理解するように、徹底的に復習すること！‼️
+let testCase = Int(readLine()!)!
+var pageArray = [String]()
+var commandArray2 = [String]()
+var beforePageIndex = 0, currentPageIndex = 0
+var resultPage: [String] = []
+var beforePage = ""
+
+for _ in 0..<testCase {
+    var pageName = ""
+    let command = readLine()!
+    commandArray2.append(command)
+    
+    var command_dividing = command.split(separator: " ").map { String($0) }
+    
+    for _ in 0..<2 {
+        command_dividing.removeFirst()
+    }
+    
+    if command != "use the back button" {
+        pageName = command_dividing.joined(separator: " ")
+        pageArray.append(pageName)
     }
 }
 
-mountain_top.sort(by: >)
-
-for i in 0..<mountain_top.count {
-    print(mountain_top[i])
+for i in 0..<commandArray2.count {
+    netSurfing(commandArray2[i])
 }
 
-func Find_Top(_ rowStart: Int, _ columnStart: Int) -> Bool {
-    let compareValue = mapData[rowStart][columnStart]
-    var isTop = true
-    
-    for i in 0..<4 {
-        let nextRow = rowStart + directionRow[i]
-        let nextColumn = columnStart +  directionColumn[i]
-        
-        if nextRow < 0 || nextRow >= mapSize || nextColumn < 0 || nextColumn >= mapSize {
-            continue
+for i in 0..<resultPage.count {
+    print(resultPage[i])
+}
+
+func netSurfing(_ string: String ) {
+    if string.contains("go to") {
+        for i in 0..<pageArray.count {
+            if string.contains(pageArray[i]) {
+                resultPage.append(pageArray[i])
+                break
+            }
         }
         
-        let compareRound = mapData[nextRow][nextColumn]
-        
-        if compareValue <= compareRound {
-            isTop = false
+        beforePageIndex = currentPageIndex - 1
+            
+        if beforePageIndex < 0 {
+            beforePageIndex = 0
+            beforePage = resultPage[beforePageIndex]
+        } else {
+            //ここがエラーが発生するらしいだね...
+            beforePage = resultPage[beforePageIndex]
         }
+        print("beforePage = \(beforePage)")
+        currentPageIndex += 1
+        
+    } else if string.contains("use the") {
+        resultPage.append(beforePage)
+        currentPageIndex = beforePageIndex
+        
     }
-    
-    return isTop
 }
