a0601d90465f03f05a14d60f9592df67ad39e0bb
KyusokLee <kyusok1@naver.com>
2022-05-14 23:53:51 +0900

    Atcoder Beginner Contest 251 _ not completed

--- AtCoder/AtCoder/main.swift
+++ AtCoder/AtCoder/main.swift
@@ -30,3 +30,144 @@ import Foundation
 //let prac_Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //let prac_filter = prac_Array.filter { $0 % 2 == 0 } // filterの中のClosureの式に当てはまる要素だけ取り出して配列にする
 //print(prac_filter)
+
+////AtCoder Beginner Contest 251
+////A.
+//let input = readLine()!
+//var result = ""
+//if !conditionCheck(input) {
+//    exit(0)
+//}
+//
+//while result.count < 6 {
+//    result += input
+//}
+//print(result)
+//
+//func conditionCheck(_ target: String) -> Bool {
+//    guard 1 <= target.count && target.count <= 3 else {
+//        return false
+//    }
+//
+//    for i in target {
+//        if Int(String(i)) != nil {
+//            return false
+//        }
+//    }
+//
+//    let lowerStr = target.lowercased()
+//
+//    if target != lowerStr {
+//        return false
+//    }
+//
+//    return true
+//}
+
+
+////B. 処理時間が結構かかった
+//let data = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let nums = data[0], W = data[1]
+//let itemArray = readLine()!.split(separator: " ").map { Int(String($0))! }
+//var checked = Array(repeating: false, count: nums)
+//var sumArray: Set<Int> = []
+//
+//if nums == 1 {
+//    print(itemArray.first! <= W ? 1 : 0)
+//} else {
+//    for i in 1...3 {
+//        if nums < i {
+//            break
+//        }
+//        combination(i, 0, 0, 0)
+//    }
+//    print(sumArray.count)
+//}
+//
+//func combination(_ depth: Int, _ level: Int, _ sum: Int, _ startIndex: Int) {
+//    if level == depth {
+//        sumArray.insert(sum)
+//        return
+//    }
+//
+//    for i in startIndex..<itemArray.count {
+//        if !checked[i] {
+//            if sum + itemArray[i] > W {
+//                continue
+//            }
+//            checked[i] = true
+//            combination(depth, level + 1, sum + itemArray[i], startIndex + 1)
+//            checked[i] = false
+//        }
+//    }
+//}
+
+////Brute Forceで探す方法 　-> より早い処理時間 (実行するテストケースが少ないから)
+//let data = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let (N, W) = (data[0], data[1])
+//let numArray = readLine()!.split(separator: " ").map { Int(String($0))! }
+//
+//print(findCountOfNiceNum())
+//
+//func findCountOfNiceNum() -> Int {
+//    var result: Set<Int> = []
+//    for i in 0..<N {
+//        var sum = numArray[i]
+//        if sum <= W {
+//            result.insert(sum)
+//        } else {
+//            continue
+//        }
+//        for j in i + 1..<N {
+//            sum = numArray[i] + numArray[j]
+//            if sum <= W {
+//                result.insert(sum)
+//            } else {
+//                continue
+//            }
+//            for k in j + 1..<N {
+//                sum = numArray[i] + numArray[j] + numArray[k]
+//                if sum <= W {
+//                    result.insert(sum)
+//                } else {
+//                    continue
+//                }
+//            }
+//        }
+//    }
+//    return result.count
+//}
+
+
+//C. 時間超過になったコード -> なんで、時間超過になるんだろう
+let testCases = Int(readLine()!)!
+var strArray = [String]()
+var pointArray = [Int]()
+var point = 0
+var result = 0
+
+for _ in 0..<testCases {
+    let input = readLine()!.split(separator: " ").map { String($0) }
+    strArray.append(input[0])
+    pointArray.append(Int(input[1])!)
+}
+
+var setArray = [String]()
+
+for i in 0..<testCases {
+    if setArray.contains(strArray[i]) {
+        continue
+    }
+    setArray.append(strArray[i])
+    if point < pointArray[i] {
+        point = pointArray[i]
+        result = i + 1
+    }
+}
+
+print(result)
+
+
+
+//D.
+
---
+++ BaekJoon/Tree アルゴリズム.swift
@@ -0,0 +1,8 @@
+//
+//  Tree アルゴリズム.swift
+//  BaekJoon
+//
+//  Created by Kyus'lee on 2022/05/14.
+//
+
+import Foundation
